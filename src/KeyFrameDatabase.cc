ef12162428a5f84e2787669559dc15e7793421db
Sean ORourke <coulpaul98@gmail.com>
2024-04-16 05:59:07 -0400

    Make core objects be shared_ptr's

--- src/KeyFrameDatabase.cc
+++ src/KeyFrameDatabase.cc
@@ -30,8 +30,8 @@
 
 namespace MORB_SLAM {
 
-KeyFrameDatabase::KeyFrameDatabase(const ORBVocabulary& voc) : mpVoc(&voc) {
-  mvInvertedFile.resize(voc.size());
+KeyFrameDatabase::KeyFrameDatabase(std::shared_ptr<ORBVocabulary> voc) : mpVoc(std::move(voc)) {
+  mvInvertedFile.resize(mpVoc->size());
 }
 
 void KeyFrameDatabase::add(KeyFrame* pKF) {
@@ -795,10 +795,8 @@ std::vector<KeyFrame*> KeyFrameDatabase::DetectRelocalizationCandidates(Frame* F
   return vpRelocCandidates;
 }
 
-void KeyFrameDatabase::SetORBVocabulary(ORBVocabulary* pORBVoc) {
-  ORBVocabulary** ptr;
-  ptr = (ORBVocabulary**)(&mpVoc);
-  *ptr = pORBVoc;
+void KeyFrameDatabase::SetORBVocabulary(std::shared_ptr<ORBVocabulary> pORBVoc) {
+  mpVoc = std::move(pORBVoc);
 
   mvInvertedFile.clear();
   mvInvertedFile.resize(mpVoc->size());
