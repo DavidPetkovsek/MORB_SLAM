bc25a4b915e28b60107355f38514c5d4681b12ee
David <76574620+chungdavid@users.noreply.github.com>
2024-11-04 09:54:26 -0500

    Remove old config file for EuRoC testing program and extenal_imu_client

--- Examples/Stereo-Inertial/run_websocket.sh
+++ Examples/Stereo-Inertial/run_websocket.sh
@@ -1,1 +1,1 @@
-gdb --args ../../bin/external_IMU_client ../../Vocabulary/ORBvoc.txt ./System_RealSense_D435i.yaml ./RealSense_D435i.yaml ./InertialOdometry_RealSense_D435i.yaml ./websocket.yaml
+gdb --args ../../bin/external_IMU_client ../../Vocabulary/ORBvoc.txt ./System_RealSense_D435i.yaml ./Camera_RealSense_D435i.yaml ./InertialOdometry_RealSense_D435i.yaml ./websocket.yaml
