d85f15b96b2a8d86d1859c0413169c79f9b0b073
David <76574620+chungdavid@users.noreply.github.com>
2024-11-11 11:32:40 -0500

    WIP: put ExternalFrameData and InertialOdometrySettings in a seperate file

---
+++ include/MORB_SLAM/InertialOdometry/ExternalFrameData.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "MORB_SLAM/Frame.h"
+#include "MORB_SLAM/InertialOdometry/ImuTypes.h"
+
+namespace MORB_SLAM {
+
+
+struct InertialFrameData : public ExternalFrameData {
+    // default constructor
+    InertialFrameData()
+        : mpImuPreintegrated(nullptr),
+          mpImuPreintegratedFrame(nullptr),
+          mbImuPreintegrated(false)
+    {
+        mpMutexImu = std::make_shared<std::mutex>();
+    }
+
+    InertialFrameData(IMU::Calib imuCalib)
+        : mImuCalib(imuCalib),
+          mpImuPreintegrated(nullptr),
+          mpImuPreintegratedFrame(nullptr),
+          mbImuPreintegrated(false)
+    {
+        mpMutexImu = std::make_shared<std::mutex>();
+    } 
+    IMU::Bias mImuBias;
+    IMU::Calib mImuCalib;
+    std::shared_ptr<IMU::Preintegrated> mpImuPreintegrated;
+    std::shared_ptr<IMU::Preintegrated> mpImuPreintegratedFrame;
+    bool mbImuPreintegrated;
+    std::shared_ptr<std::mutex> mpMutexImu;
+};
+
+
+} // namespace MORB_SLAM
--- include/MORB_SLAM/InertialOdometry/InertialOdometry.hpp
+++ include/MORB_SLAM/InertialOdometry/InertialOdometry.hpp
@@ -1,74 +1,16 @@
 #pragma once
 
 #include "MORB_SLAM/Odometry.hpp"
-#include "MORB_SLAM/InertialOdometry/ImuTypes.h"
-#include "MORB_SLAM/Settings/Settings.h"
-#include "MORB_SLAM/Frame.h"
+#include "MORB_SLAM/InertialOdometry/InertialOdometrySettings.hpp"
+#include "MORB_SLAM/InertialOdometry/ExternalFrameData.hpp"
 
 #include <Eigen/Core>
-
 #include <mutex>
 
 namespace MORB_SLAM {
 
 
 class Atlas;
-
-struct InertialFrameData : public ExternalFrameData {
-    // default constructor
-    InertialFrameData()
-        : mpImuPreintegrated(nullptr),
-          mpImuPreintegratedFrame(nullptr),
-          mbImuPreintegrated(false)
-    {
-        mpMutexImu = std::make_shared<std::mutex>();
-    }
-
-    InertialFrameData(IMU::Calib imuCalib)
-        : mImuCalib(imuCalib),
-          mpImuPreintegrated(nullptr),
-          mpImuPreintegratedFrame(nullptr),
-          mbImuPreintegrated(false)
-    {
-        mpMutexImu = std::make_shared<std::mutex>();
-    } 
-    IMU::Bias mImuBias;
-    IMU::Calib mImuCalib;
-    std::shared_ptr<IMU::Preintegrated> mpImuPreintegrated;
-    std::shared_ptr<IMU::Preintegrated> mpImuPreintegratedFrame;
-    bool mbImuPreintegrated;
-    std::shared_ptr<std::mutex> mpMutexImu;
-};
-
-class InertialOdometrySettings : public Settings {
-public:
-    InertialOdometrySettings(const std::string& configFile);
-
-    float noiseGyro() const { return noiseGyro_; }
-    float noiseAcc() const { return noiseAcc_; }
-    float gyroWalk() const { return gyroWalk_; }
-    float accWalk() const { return accWalk_; }
-    float accFrequency() const { return accFrequency_; }
-    float gyroFrequency() const { return gyroFrequency_; }
-    const Sophus::SE3f &Tbc() const { return Tbc_; }
-
-    bool fastIMUInit() const { return fastIMUInit_; }
-    bool stationaryIMUInit() const { return stationaryIMUInit_; }
-
-    friend std::ostream& operator<<(std::ostream& output, const InertialOdometrySettings& s);
-
-private:
-    void readIMU(cv::FileStorage& fSettings);
-
-    float noiseGyro_, noiseAcc_;
-    float gyroWalk_, accWalk_;
-    float accFrequency_;
-    float gyroFrequency_;
-    Sophus::SE3f Tbc_;
-
-    bool fastIMUInit_;
-    bool stationaryIMUInit_;
-};
 
 class InertialOdometry : public Odometry {
 
