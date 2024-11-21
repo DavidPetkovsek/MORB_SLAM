d959eaa51afd19f9e8d002ea17c4b21b70b56aa8
David <76574620+chungdavid@users.noreply.github.com>
2024-11-21 16:36:42 -0500

    Clean Odometry: Remove unused Tracking methods

--- include/MORB_SLAM/Odometry.hpp
+++ include/MORB_SLAM/Odometry.hpp
@@ -106,14 +106,6 @@ protected:
     std::shared_ptr<Atlas> mpAtlas;
     std::weak_ptr<Tracking> mwpTracker;
 
-    // TO-DO: Remove. Tracker was initially a private member of Odometry, so any child classes can't access it. This has been changed, so the below methods aren't needed anymore.
-    void TrackingLockPreTeleportTranslation(bool is_locked);
-    void TrackingSetTeleported(bool is_teleported);
-    // void TrackingUpdateFrameOdom(const float s, const IMU::Bias& b, std::shared_ptr<KeyFrame> curr_kf); // TODO: Rework this
-    void TrackingSetState(TrackingState state);
-    TrackingState TrackingGetState();
-    std::shared_ptr<KeyFrame> TrackingGetLastKeyFrame();
-
     // Local Mapping
     bool LocalMappingResetRequested();
     void LocalMappingSetInitializing(bool is_initializing);
--- src/Odometry.cpp
+++ src/Odometry.cpp
@@ -10,14 +10,6 @@ namespace MORB_SLAM {
     void Odometry::SetTracker(const std::shared_ptr<Tracking> &pTracker) { mwpTracker = pTracker; };
     void Odometry::SetAtlas(const std::shared_ptr<Atlas> &pAtlas) { mpAtlas = pAtlas; };
 
-    // Tracking
-    void Odometry::TrackingLockPreTeleportTranslation(bool is_locked) { std::shared_ptr<Tracking> pTracker = mwpTracker.lock(); pTracker->mLockPreTeleportTranslation = is_locked; }
-    void Odometry::TrackingSetTeleported(bool is_teleported) { std::shared_ptr<Tracking> pTracker = mwpTracker.lock(); pTracker->mLockPreTeleportTranslation = is_teleported; }
-    // void Odometry::TrackingUpdateFrameOdom(const float s, const IMU::Bias& b, std::shared_ptr<KeyFrame> curr_kf) { std::shared_ptr<Tracking> pTracker = mwpTracker.lock(); pTracker->UpdateFrameIMU(s, b, curr_kf); }
-    void Odometry::TrackingSetState(TrackingState state) { std::shared_ptr<Tracking> pTracker = mwpTracker.lock(); pTracker->mState = state; }
-    TrackingState Odometry::TrackingGetState() { std::shared_ptr<Tracking> pTracker = mwpTracker.lock(); return pTracker->mState; }
-    std::shared_ptr<KeyFrame> Odometry::TrackingGetLastKeyFrame() { std::shared_ptr<Tracking> pTracker = mwpTracker.lock(); return pTracker->GetLastKeyFrame(); }
-
     // Local Mapping
     bool Odometry::LocalMappingResetRequested() { std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock(); return pLocalMapper->mbResetRequested; }
     void Odometry::LocalMappingSetInitializing(bool is_initializing) { std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock(); pLocalMapper->bInitializing = is_initializing; }
