d419402bf666352ff2306d6ba8466c2d2a37334c
David <76574620+chungdavid@users.noreply.github.com>
2024-11-26 11:37:11 -0500

    Throw excpetions if the LocalMapping object does not exist (this should never happen)

--- src/Odometry.cpp
+++ src/Odometry.cpp
@@ -15,35 +15,72 @@ namespace MORB_SLAM {
         if (std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock())
             return pLocalMapper->mbResetRequested;
         else
-            return false;    
+            throw std::runtime_error("ERROR: Cannot access 'LocalMapping' object because it has been destroyed.");
     }
-    void Odometry::LocalMappingSetInitializing(bool is_initializing) { if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock()) pLocalMapper->bInitializing = is_initializing; }
+
+    void Odometry::LocalMappingSetInitializing(bool is_initializing) {
+        if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock())
+            pLocalMapper->bInitializing = is_initializing;
+        else
+            throw std::runtime_error("ERROR: Cannot access 'LocalMapping' object because it has been destroyed.");
+    }
+
     void Odometry::LocalMappingProcessKeyFramesInQueue(std::vector<std::shared_ptr<KeyFrame>> &vpKF) {
         if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock()) {
             while (pLocalMapper->CheckNewKeyFrames()) {
                 pLocalMapper->ProcessNewKeyFrame();
                 vpKF.push_back(pLocalMapper->mpCurrentKeyFrame);
             }
+        } else {
+            throw std::runtime_error("ERROR: Cannot access 'LocalMapping' object because it has been destroyed.");
         }
     }
-    void Odometry::LocalMappingSetTimeInit(float t_init) { if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock()) pLocalMapper->mTinit = t_init; }
+
+    void Odometry::LocalMappingSetTimeInit(float t_init) {
+        if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock())
+            pLocalMapper->mTinit = t_init;
+        else
+            throw std::runtime_error("ERROR: Cannot access 'LocalMapping' object because it has been destroyed.");
+    }
+
     float Odometry::LocalMappingGetTimeInit() {
         if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock())
             return pLocalMapper->mTinit;
         else
-            return 0;
+            throw std::runtime_error("ERROR: Cannot access 'LocalMapping' object because it has been destroyed.");
     }
-    void Odometry::LocalMappingIncrementTimeInit(float t_increment) { if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock()) pLocalMapper->mTinit += t_increment; }
-    void Odometry::LocalMappingSetPoseReverseAxisFlip(Sophus::SE3f pose) { if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock()) pLocalMapper->mPoseReverseAxisFlip = pose; }
+
+    void Odometry::LocalMappingIncrementTimeInit(float t_increment) {
+        if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock())
+            pLocalMapper->mTinit += t_increment;
+        else
+            throw std::runtime_error("ERROR: Cannot access 'LocalMapping' object because it has been destroyed.");
+    }
+
+    void Odometry::LocalMappingSetPoseReverseAxisFlip(Sophus::SE3f pose) {
+        if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock())
+            pLocalMapper->mPoseReverseAxisFlip = pose;
+        else
+            throw std::runtime_error("ERROR: Cannot access 'LocalMapping' object because it has been destroyed.");
+    }
+
     void Odometry::LocalMappingSetNewKeyFramesBad() {
         if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock()) {
             for (std::shared_ptr<KeyFrame> newKeyFrame : pLocalMapper->mlNewKeyFrames) {
                 newKeyFrame->SetBadFlag();
             }
             pLocalMapper->mlNewKeyFrames.clear();
+        } else {
+            throw std::runtime_error("ERROR: Cannot access 'LocalMapping' object because it has been destroyed.");
         }
     }
-    std::shared_ptr<KeyFrame> Odometry::LocalMappingGetCurrentKeyFrame() { if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock()) return pLocalMapper->mpCurrentKeyFrame; }
+
+    std::shared_ptr<KeyFrame> Odometry::LocalMappingGetCurrentKeyFrame() {
+        if(std::shared_ptr<LocalMapping> pLocalMapper = mwpLocalMapper.lock())
+            return pLocalMapper->mpCurrentKeyFrame;
+        else
+            throw std::runtime_error("ERROR: Cannot access 'LocalMapping' object because it has been destroyed.");
+    }
 
 
 } //namespace MORB_SLAM
