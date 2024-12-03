7585e873729b4750164949d7de163f44958d8b55
David <76574620+chungdavid@users.noreply.github.com>
2024-12-03 09:31:58 -0500

    Make the y axis point up in the top view

--- src/Viewer.cc
+++ src/Viewer.cc
@@ -262,7 +262,7 @@ void Viewer::Run() {
       menuTopView = false;
       bCameraView = false;
       s_cam.SetProjectionMatrix(pangolin::ProjectionMatrix(1024, 768, 3000, 3000, 512, 389, 0.1, 10000));
-      s_cam.SetModelViewMatrix(pangolin::ModelViewLookAt(0, 0.01, 50, 0, 0, 0, 0.0, 0.0, 1.0));
+      s_cam.SetModelViewMatrix(pangolin::ModelViewLookAt(0, 0.01, 50, 0, 0, 0, 0.0, 1.0, 0.0));
       s_cam.Follow(Ow);
     }
 
