75c5fe06fdb4ef7f141fbd9dfca99314ea9e9d3f
David <76574620+chungdavid@users.noreply.github.com>
2024-12-12 12:59:02 -0500

    Change external_map_viewer2.py client to plot odometry data from SLAM, add exception handling and program exit

--- scripts/external_map_viewer2.py
+++ scripts/external_map_viewer2.py
@@ -3,51 +3,40 @@ import cv2
 import websockets
 import numpy as np
 import matplotlib.pyplot as plt
-from threading import Thread, Lock
+from threading import Thread, Lock, Event
 
 def parse_binary_data(binary_data):
         isFromSLAM = binary_data[0]
         if isFromSLAM==1:
             # rotation_matrix = np.frombuffer(binary_data[1:37], dtype=np.float32).reshape(3, 3)
-            
-            translation_vector = np.frombuffer(binary_data[37:49], dtype=np.float32)
-            # state = struct.unpack('<i', binary_data[49:53])[0]
-            # message = struct.unpack('<i', binary_data[53:57])[0]
-            # isKF = struct.unpack('<?', binary_data[57:58])[0]
-            # return isFromSLAM, rotation_matrix, translation_vector, state, message, isKF
-            return isFromSLAM, translation_vector
+            pose_data = np.frombuffer(binary_data, dtype=np.float32, count=6, offset=37)
+            return isFromSLAM, pose_data
         else:
-            translation_vector = np.frombuffer(binary_data[1:13], dtype=np.float32)
-            # return isFromSLAM, None, translation_vector, None, None, None
-            return isFromSLAM, translation_vector
+            pose_data = np.frombuffer(binary_data, dtype=np.float32, count=3, offset=1)
+            return isFromSLAM, pose_data
 
-async def main():
+async def main(event: Event):
     uri = "ws://192.168.1.1:9002"
-
     try:
         async with websockets.connect(uri, ping_interval=None) as websocket:
-            n = 0
-            n2 = 0
-            c = 3
-            while not shouldClose:
-                # async with asyncio.timeout(timeout=1.5):
+            print(f"Connected to WebSocket server: {uri}")
+
+            while not event.is_set():
                 binary_data = await websocket.recv()
-                isFromSLAM, translation_vector = parse_binary_data(binary_data)
-                n += 1
-                n2 += 1
+                isFromSLAM, pose_data = parse_binary_data(binary_data)
+
                 if isFromSLAM:
-                    if n > c:
-                        if p_SLAM.update(translation_vector[0], translation_vector[1], False):
-                            n=0
+                    p_SLAM.update(pose_data[0], pose_data[1], pose_data[3], pose_data[4], doDraw=False)
+
                 else:
-                    if n2 > c:
-                        if p_AIV.update(translation_vector[0], translation_vector[1], False):
-                            n2=0
-                
-    except:
-        import traceback
-        traceback.print_exc()
-        
+                    p_AIV.update(pose_data[0], pose_data[1], doDraw=False)
+
+    except websockets.exceptions.ConnectionClosed as e:
+        print(e)
+    except Exception as e:
+        print(f"Error processing socket message: {e}")
+    finally:
+        print("WebSocket closed.")
 
 def minMax(minX, maxX, minY, maxY, padding=0.1, numTicks=10):
     dx = maxX-minX
@@ -71,9 +60,12 @@ class fancy_matplot:
     def __init__(self, name=None):
         self.fig = plt.figure(figsize=(8,8))
         self.ax = self.fig.add_subplot(111)
-        self.Ln, = self.ax.plot([0,0])
-        self.datX=[]
-        self.datY=[]
+        self.pos, = self.ax.plot([], [], label="Position")
+        self.odom, = self.ax.plot([], [], label="Odometry")
+        self.pos_datX=[]
+        self.pos_datY=[]
+        self.odom_datX=[0]
+        self.odom_datY=[0]
         self.lock = Lock()
         if not name is None:
             self.ax.set_title(name)
@@ -82,37 +74,44 @@ class fancy_matplot:
         self.minY = float('inf')
         self.maxY = -float('inf')
         self.updated = False
+        self.ax.legend()
 
-    def update(self, valX, valY, doDraw=True):
+    def update(self, pos_valX, pos_valY, odom_valX=0, odom_valY=0, doDraw=True):
         updated = False
         with self.lock:
-            if valX < self.minX: self.minX = valX
-            if valY < self.minY: self.minY = valY
-            if valX > self.maxX: self.maxX = valX
-            if valY > self.maxY: self.maxY = valY
-            # print(np.sqrt((valX-self.datX[-1])**2 + (valY-self.datY[-1])**2))
-            if len(self.datX) < 4 or len(self.datY) < 4 or (not (np.sqrt((valX-self.datX[-1])**2 + (valY-self.datY[-1])**2) < 0.0001)):
-                self.datX.append(valX)
-                self.datY.append(valY)
+            if pos_valX < self.minX: self.minX = pos_valX
+            if pos_valY < self.minY: self.minY = pos_valY
+            if pos_valX > self.maxX: self.maxX = pos_valX
+            if pos_valY > self.maxY: self.maxY = pos_valY
+            # print(np.sqrt((valX-self.pos_datX[-1])**2 + (valY-self.datY[-1])**2))
+            if len(self.pos_datX) < 4 or len(self.pos_datY) < 4 or (not (np.sqrt((pos_valX-self.pos_datX[-1])**2 + (pos_valY-self.pos_datY[-1])**2) < 0.0001)):
+                self.pos_datX.append(pos_valX)
+                self.pos_datY.append(pos_valY)
+
+                self.odom_datX.append(self.odom_datX[-1] + odom_valX)
+                self.odom_datY.append(self.odom_datY[-1] + odom_valY)
+
                 self.updated = True
                 updated = True
+        
         if doDraw:
             self.draw()
+        
         return updated
 
     def draw(self):
-
         with self.lock:
-
             if self.updated:
-                
-                self.Ln.set_ydata(self.datY)
-                self.Ln.set_xdata(self.datX)
+                self.pos.set_ydata(self.pos_datY)
+                self.pos.set_xdata(self.pos_datX)
+                self.odom.set_ydata(self.odom_datY)
+                self.odom.set_xdata(self.odom_datX)
                 xr, yr = minMax(self.minX, self.maxX, self.minY, self.maxY)
                 self.ax.set_xticks(xr)
                 self.ax.set_yticks(yr)
                 self.fig.canvas.draw()
                 self.updated = False
+
     def getImage(self):
         with self.lock:
             img = np.fromstring(self.fig.canvas.tostring_rgb(), dtype=np.uint8,sep='')
@@ -120,22 +119,27 @@ class fancy_matplot:
             img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
         return img
 
-p_SLAM = fancy_matplot("SLAM")
-p_AIV = fancy_matplot("AIV")
-shouldClose = False
-thread = Thread(target=lambda:asyncio.run(main()))
-thread.start()
-# asyncio.run(main())
-while True:
-    p_SLAM.draw()
-    p_AIV.draw()
-    if thread.is_alive():
-        plt.pause(0.1)
-    else:
-        break
+if __name__ == "__main__":
+    p_SLAM = fancy_matplot("SLAM")
+    p_AIV = fancy_matplot("AIV")
 
-shouldClose = True
-plt.pause(0)
-print('waiting for websocket to join...')
-thread.join()
-print('all done :)')
+    event = Event()
+    thread = Thread(target=lambda:asyncio.run(main(event)))
+    thread.start()
+
+    # asyncio.run(main())
+    try:
+        while True:
+            p_SLAM.draw()
+            p_AIV.draw()
+            if thread.is_alive():
+                plt.pause(0.1)
+            else:
+                break
+    except:
+        event.set()
+    finally:
+        plt.close()
+        print('waiting for websocket to join...')
+        thread.join()
+        print('all done :)')
