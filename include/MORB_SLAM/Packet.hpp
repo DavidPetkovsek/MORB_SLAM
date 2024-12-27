a07c65bdb48e487240125737ee3cc36314ef46ea
David <76574620+chungdavid@users.noreply.github.com>
2024-12-26 20:04:17 -0500

    update how Packet is contructed - include trackingState and mapUpdated in the constructor

--- include/MORB_SLAM/Packet.hpp
+++ include/MORB_SLAM/Packet.hpp
@@ -12,12 +12,13 @@
 namespace MORB_SLAM{
 
 struct Packet {
+    TrackingState trackingState;
+    bool mapUpdated; // did a LocalBA, GBA, MapMerge, or LoopClose occur right before this frame?
     std::optional<Sophus::SE3f> mapPose; // Tcw, Transformation from world to camera frame
     std::optional<Sophus::SE3f> deltaPose; // Tc1c2, Transformation from last frame to current frame, where c1 is current frame and c2 is last frame
-    TrackingState state = TrackingState::SYSTEM_NOT_READY;
-    bool mapUpdated; // occurs after a LocalBA, GBA, MapMerge, LoopClose
 
-    Packet(std::optional<Sophus::SE3f> mapPose = std::nullopt, std::optional<Sophus::SE3f> deltaPose = std::nullopt);
+    Packet();
+    Packet(const TrackingState &trackingState, const bool &mapUpdated, std::optional<Sophus::SE3f> mapPose = std::nullopt, std::optional<Sophus::SE3f> deltaPose = std::nullopt);
     virtual ~Packet();
 };
 struct InertialPacket{};
@@ -26,20 +27,19 @@ struct StereoPacket : public Packet, public InertialPacket {
     cv::Mat imgLeft;
     cv::Mat imgRight;
     StereoPacket(const cv::Mat &imgLeft, const cv::Mat &imgRight);
-    StereoPacket(const Sophus::SE3f &mapPose, const cv::Mat &imgLeft, const cv::Mat &imgRight);
-    StereoPacket(const Sophus::SE3f &mapPose, const Sophus::SE3f &deltaPose, const cv::Mat &imgLeft, const cv::Mat &imgRight);
+    StereoPacket(const TrackingState &trackingState, const bool &mapUpdated, const cv::Mat &imgLeft, const cv::Mat &imgRight);
 };
 
 struct MonoPacket : public Packet, public InertialPacket {
     cv::Mat img;
     MonoPacket(const cv::Mat &img);
-    MonoPacket(const Sophus::SE3f &pose, const cv::Mat &img);
+    MonoPacket(const TrackingState &trackingState, const bool &mapUpdated, const cv::Mat &img);
 };
 
 struct RGBDPacket : public Packet, public InertialPacket {
     cv::Mat img;
     cv::Mat depthImg;
     RGBDPacket(const cv::Mat &img, const cv::Mat &depthImg);
-    RGBDPacket(const Sophus::SE3f &pose, const cv::Mat &img, const cv::Mat &depthImg);
+    RGBDPacket(const TrackingState &trackingState, const bool &mapUpdated, const cv::Mat &img, const cv::Mat &depthImg);
 };
 }
